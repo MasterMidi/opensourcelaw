2029df788af7d25aa7dd87d44af738334ae1b564
Michael Andreas Graversen <home@michael-graversen.dk>
2026-07-06 23:03:55 +0200

    setting context

--- src/learning.py
+++ src/learning.py
@@ -1,11 +1,23 @@
-from dagster import asset
+from dagster import AssetExecutionContext, asset
 
 
 @asset(group_name="learning")
-def hello_dagster() -> str:
-    return "Hello from Dagster"
+def hello_dagster(context: AssetExecutionContext) -> str:
+    message = "Hello from Dagster"
+    context.log.info("Created the hello message")
+    context.add_output_metadata({"message": message, "message_length": len(message)})
+    return message
 
 
 @asset(group_name="learning")
-def excited_hello(hello_dagster: str) -> str:
-    return hello_dagster.upper() + "!"
+def excited_hello(context: AssetExecutionContext, hello_dagster: str) -> str:
+    excited_message = hello_dagster.upper() + "!"
+    context.log.info("Created the excited hello message")
+    context.add_output_metadata(
+        {
+            "original_message": hello_dagster,
+            "excited_message": excited_message,
+            "excited_message_length": len(excited_message),
+        }
+    )
+    return excited_message
